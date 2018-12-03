8e1922cc3eb5f189ebe383619b7fb15f13a6234a
laurence <laurencehall1@outlook.com>
2018-12-03 18:03:49 -0500

    testing io.connect change

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/', function (req, res) {
 
           .then(($) => {
            searchResult = $('body').find(data.keyword)
-           fs.writeFile('response.pdf', searchResult.text(), (err) => {
+           fs.writeFile('response.doc', searchResult.text(), (err) => {
                if(err) throw err
             console.log('response written')
            
