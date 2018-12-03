fc255702def22b0b02d8a03ff445285d37261ec9
laurence <laurencehall1@outlook.com>
2018-12-03 17:30:49 -0500

    Added GET to /download for users to have a downloadable doc of their search results

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const rp = require('request-promise');
 const cheerio = require('cheerio');
-var app = require('express')();
-var server = require('http').createServer(app);  
-var io = require('socket.io')(server);var fs = require('fs');
+const http = require('http');
+const app = require('express')();
+const server = require('http').createServer(app);  
+const io = require('socket.io')(server);var fs = require('fs');
 
-var port = process.env.port || 3000;
+const port = process.env.port || 3000;
 server.listen(port, () => console.log(`Server running on ${port}`));
 
-var options = {
+const options = {
     uri: null,
     transform: function (body) {
       return cheerio.load(body);
@@ -24,19 +25,25 @@ app.get('/', function (req, res) {
 
           .then(($) => {
            searchResult = $('body').find(data.keyword)
-           fs.writeFile('response', searchResult.text(), (err) => {
+           fs.writeFile('response.pdf', searchResult.text(), (err) => {
                if(err) throw err
             console.log('response written')
            
-             socket.emit('searchResult', searchResult)
+             socket.emit('searchResult')
             })
   
               
         }).catch(err =>socket.emit('err', err.message))
       });
+     
     
     });
   });
+  app.get('/download', (req, res) => {
+    console.log('req made')
+    let file = __dirname + '/response.doc'
+    res.download(file)
+  })
   
 
 
