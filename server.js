3a92e191bd87f903d1382f9fde89698acfcd9e8f
laurence <laurencehall1@outlook.com>
2018-12-10 21:05:18 -0500

    added security features with helmetjs

--- server.js
+++ server.js
@@ -1,50 +1,61 @@
 const rp = require('request-promise');
 const cheerio = require('cheerio');
+const helmet = require('helmet');
 const http = require('http');
 const app = require('express')();
-const server = require('http').createServer(app);  
-const io = require('socket.io')(server);var fs = require('fs');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+var fs = require('fs');
+server.timeout = 1000;
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => console.log(`Server running on ${port}`));
 
 const options = {
-    uri: null,
-    transform: function (body) {
-      return cheerio.load(body);
-    }
-  };
+  uri: null,
+  transform: function (body) {
+    return cheerio.load(body);
+  }
+};
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-    io.on('connection', function (socket) {
+app.get('/', function async (req, res) {
+  res.sendFile(__dirname + '/index.html');
+  io.on('connection', function (socket) {
 
-      socket.on('searchRequest', function (data) {
-          options.uri = data.url;
-          rp(options)
+    socket.on('searchRequest', function (data) {
+      options.uri = data.url;
+      rp(options)
 
-          .then(($) => {
-           searchResult = $('body').find(data.keyword)
-           fs.writeFile('response.doc', searchResult.text(), (err) => {
-               if(err) throw err
+        .then(($) => {
+          // searchResult = $('body').find(data.keyword)
+          searchResult = $(`*:contains(${data.keyword})`)
+          fs.writeFile('response.doc', searchResult.text(), (err) => {
+            if (err) throw err
             console.log('response written')
-           
-             socket.emit('searchResult')
-            })
-  
-              
-        }).catch(err =>socket.emit('err', err.message))
-      });
-     
-    
+
+            socket.emit('searchResult')
+          })
+
+
+        }).catch(err => socket.emit('err', err.message))
     });
+
+
   });
-  app.get('/download', (req, res) => {
-    console.log('req made')
-    let file = __dirname + '/response.doc'
-    res.download(file)
-  })
-  
+});
 
+app.get('/download', (req, res) => {
+  console.log('req made for download')
+  let file = __dirname + '/response.doc'
+  res.download(file)
+})
 
-  
+app.get('/webview', (req, res) => {
+  console.log('req made for html view')
+  let file = __dirname + '/response.doc'
+  try {
+    res.sendFile(file)
+  } catch (err) {
+    res.write('Error')
+  }
+})
